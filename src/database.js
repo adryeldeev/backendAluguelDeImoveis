27a13e031e52b7e0f44be10dfe81398b9ca22849
adryel martins <adryeldev@hotmail.com>
2024-04-17 16:32:56 -0300

    deploy

--- src/database.js
+++ src/database.js
@@ -1,34 +1,25 @@
+import { createPool } from "mysql2";
 
-import mysql from 'mysql2'
-const db =  mysql.createConnection({
-    host: "viaduct.proxy.rlwy.net",
-    user: "root",
-    port: 25760,
-    password: "RuXnKeyytJqWvgyJCPOFsWqcShahoHyZ",
-    database: "railway"
-});
+const pool = createPool({
+    host:"viaduct.proxy.rlwy.net",
+    user:"root",
+    port:25760,
+    password:"RuXnKeyytJqWvgyJCPOFsWqcShahoHyZ",
+    database:"railway",
+    connectionLimit:10
+    
+})
 
-db.connect(err => {
+pool.query(`SHOW TABLES`, (err, result, fields) => {
     if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err.stack);
-        return;
+        return console.log(err);
     }
-
-    console.log('Conexão bem-sucedida ao banco de dados.');
-
-    db.query(`SHOW TABLES`, (err, result, fields) => {
-        if (err) {
-            console.error('Erro ao executar consulta:', err.stack);
-            return;
-        }
-
-        console.log("Tabelas no banco de dados:");
-        result.forEach(row => {
-            console.log(row.Tables_in_railway);
-        });
-
-        db.end(); // Fechar a conexão após a consulta
+    console.log("Tabelas no banco de dados:");
+    result.forEach(row => {
+        console.log(row.Tables_in_railway);
     });
 });
 
-export default db;
+
+
+export default pool
