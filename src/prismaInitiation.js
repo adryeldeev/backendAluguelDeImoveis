5da92e9e6255b9f28bef551bffc240fc0d45d98f
adryel martins <adryeldev@hotmail.com>
2024-05-08 15:59:38 -0300

    deploy

--- src/prismaInitiation.js
+++ src/prismaInitiation.js
@@ -1,8 +1,8 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('../prisma/prisma/node_modules/.prisma/client');
 
 const prisma = new PrismaClient({
   datasources: {
-    db: { url: "mysql://root:RuXnKeyytJqWvgyJCPOFsWqcShahoHyZ@viaduct.proxy.rlwy.net:25760/railway" }
+    db: { url: 'mysql://root:RuXnKeyytJqWvgyJCPOFsWqcShahoHyZ@viaduct.proxy.rlwy.net:25760/railway' }
   }
 });
 
